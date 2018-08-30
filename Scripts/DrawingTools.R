72d7a080bcb9a229fe84604939bb00be10447bcc
ArianeMirabel <ariane.mirabel@ecofog.gf>
2018-08-30 12:00:04 -0300

    relecture

--- Scripts/DrawingTools.R
+++ Scripts/DrawingTools.R
@@ -16,7 +16,7 @@ Draw_Uncertainty<-function(Gradients){
 
 
 Draw_SamplingEffort<-function(Gradient){
-  par(mar=c(0, 2.5, 2, 2))
+  par(mar=c(0, 2, 2, 1.5))
   plot(colnames(Gradient),Gradient[1,],type="l",ylim=c(0,max(Gradient)),xaxt="n",xlab="",ylab="")
   quant95<-apply(Gradient,2,function(x){quantile(x,probs=c(0.025,0.975))})
   polygon(c(colnames(Gradient),rev(colnames(Gradient))),c(pmax(quant95[1,]),pmin(rev(quant95[2,]))),col="gray75")
@@ -25,7 +25,7 @@ Draw_SamplingEffort<-function(Gradient){
   points(colnames(Gradient), apply(Gradient,2,median), col = "black",pch = 3)
   abline(h=mean(Gradient[,ncol(Gradient)]),col="red",lty=2)
   
-  par(mar=c(3.1, 2.6, 0, 2.1))
+  par(mar=c(3.1, 2.1, 0, 1.6))
   Gradient_d<-(Gradient-mean(Gradient[,ncol(Gradient)]))/mean(Gradient[,ncol(Gradient)])*100
   plot(colnames(Gradient),Gradient_d[1,],type="l",xaxt="n",xlab="",
        ylim=c(min(Gradient_d),max(Gradient_d)),ylab="")
